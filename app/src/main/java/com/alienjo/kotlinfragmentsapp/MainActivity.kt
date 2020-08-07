494670ae744dd6efa3491bbe80675b6f526e0fd2
Yousuf <josephali1993@gmail.com>
2020-08-07 17:44:24 +0400

    # MainActivity.kt implements MediatorInterface.

--- app/src/main/java/com/alienjo/kotlinfragmentsapp/MainActivity.kt
+++ app/src/main/java/com/alienjo/kotlinfragmentsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.alienjo.kotlinfragmentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MediatorInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun changeFragmentTo(fragmentToDisplay: Fragment, fragmentTag: String?) {
+        val fm = supportFragmentManager
+        val ft = fm.beginTransaction()
+        ft.replace(R.id.fl_host, fragmentToDisplay, fragmentTag)
+        if (fm.findFragmentByTag(fragmentTag) == null) {
+            ft.addToBackStack(fragmentTag)
+        }
+        ft.commit()
+    }
+
+
 }
